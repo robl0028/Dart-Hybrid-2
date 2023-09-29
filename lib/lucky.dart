4535434d85cdf3a061cae17d9362d852ba85c7ce
Samatar <robl0028@algonquinlive.com>
2023-09-28 20:08:14 -0400

    Edited var into type int

--- lib/lucky.dart
+++ lib/lucky.dart
@@ -5,8 +5,8 @@ class Lucky {
   int _luckyNumber = 0;
 
   Lucky() {
-    var min = int.parse(Platform.environment['MIN'] ?? '0');
-    var max = int.parse(Platform.environment['MAX'] ?? '100');
+    int min = int.parse(Platform.environment['MIN'] ?? '0');
+    int max = int.parse(Platform.environment['MAX'] ?? '100');
     var random = Random();
     _luckyNumber = min + random.nextInt(max - min + 1);
   }
